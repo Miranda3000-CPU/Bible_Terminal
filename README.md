9bc4083c54c8f2a2d64eb2d659e494ec5ab08a95
Miranda3000-CPU <jeiel.lima.miranda@gmail.com>
2025-11-23 14:02:21 +0000

    backup

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Bible_Terminal
-CLI
+# Biblie_Terminal
+
+Bem vindo!
+
+Bíblia funciona em maioria dos terminais baseado em Linux (Unix). Para procurar uma palavra no texto, use `/` exemplo: (/Armagedom) e dê ENTER. Use `h` para pedir ajuda quando tiveres lendo.
+
+- 🇧🇷 pt-br
+- 🌐 en
+
+Em dois tipos de idiomas estão inseridos. Funciona 💯 offline os ".txt. Experimente. Contribue, assim será possível outros países que não tem acesso a Bíblia Sagrada, mas tem acesso ao GitHub, podem baixar a Bíblia no Terminal.
+
+#
+
+# Biblie_Terminal
+
+ Welcome!
+
+ Bible works on most Linux (Unix) based terminals.  To search for a word in the text, use `/` example: (/Armageddon) and press ENTER.  Use `h` to ask for help when reading.
+
+ - 🇧🇷 pt-br
+ - 🌐 in
+
+ In two types of languages ​​are inserted.  It works 💯 offline the ".txt. Try it. Contribute, so it will be possible for other countries that do not have access to the Holy Bible, but have access to GitHub, can download the Bible in the Terminal.
---
+++ bible.py
@@ -0,0 +1,101 @@
+import os
+from colorama import init, Fore, Style
+os.system("pip install colorama")
+def menu():
+
+    print("""
+ .----------------. 
+| .--------------. |
+| |      _       | |
+| |     | |      | |
+| |  ___| |___   | |
+| | |___   ___|  | |
+| |     | |      | |
+| |     |_|      | |
+| |              | |
+| '--------------' |
+ '----------------' 
+========================================
+Por: Jetrom
+Github: Jetrom17
+Version 1.0
+----
+🤖 Queira contribuir: https://github.com/Jetrom17/TabNews_Terminal.git
+----
+==========================================
+00. E-mail para sugestões
+==========================================
+1. Biblia Sagrada pt-br
+2. Holy Bible en
+999. Baixar ferramenta "links"
+==========================================
+x. SAIR
+==========================================
+""")
+
+loop = True
+
+while loop:
+    menu()
+    what = input("#: ")
+    if what == "00":
+        print("================================")
+        print("Você terá meu contato via e-mail. Meu provedor não é Google e nem Protonmail.")
+        print("----------------")
+        hm = input("Tem certeza que queres meu contato via e-mail? (y/n): ")
+        print("================================")
+        if hm == "y":
+            print("========================================================")
+            repo = input("Opa! Antes te dá meu e-mail, posso atualizar e dá upgrade em seu repositório, se disponível? (y,n): ")
+            if repo == "y":
+                print("========================================================")
+                os.system("apt update -y")
+                os.system("apt upgrade -y")
+                os.system("clear")
+                print(Fore.GREEN + "📩 Jetrom@mail2tor.com")
+                print("========================================")
+                print("[+] Repositórios supostamente atualizados")
+                print("[+] Dado upgrade")
+                print('''
+ .----------------. 
+| .--------------. |
+| |      _       | |
+| |     | |      | |
+| |  ___| |___   | |
+| | |___   ___|  | |
+| |     | |      | |
+| |     |_|      | |
+| |              | |
+| '--------------' |
+ '----------------'                 
+''')
+                print("Agora você tem super bible!")
+                print("========================================")
+            else:
+                rmenu = input("Cancelado, voltar ao menu? (y/n): ")
+                if rmenu == "y":
+                    menu()
+    elif what == "1":
+        os.system("less AA.txt")
+        rmenu = input("Voltar ao menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "2":
+        os.system("less ACV.txt")
+        rmenu = input("Voltar ao menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "999":
+        os.system("apt install links -y")
+        rmenu = input("Voltar ao menu? (y/n): ")
+        if rmenu == "y":
+            menu()
+        else:
+            break
+    elif what == "x":
+        print("Tchau!")
+        break
